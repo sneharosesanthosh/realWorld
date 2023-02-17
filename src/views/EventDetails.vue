f69e69c93df3fcff3a3f7e793be636344df9edf8
sneha <inbox.sneha090@gmail.com>
2023-02-17 02:42:43 -0800

    new EventDetails component

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{ event.title }}</h1>
+  <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+  <p>{{ event.description }}</p>
+</template>
+
+<script>
+import EventService from "@/services/EventService.js"
+export default {
+    data () {
+        return {
+            event: null,
+        }
+    }
+};
+
+created () {
+    EventService.ApiClient.get("/events/")
+}
+</script>
+
+<style></style>
